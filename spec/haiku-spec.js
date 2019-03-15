01765fbe557955a3c5bc276bf2d1e5b51001208b
David Monarrez <davidmonarrez3@gmail.com>
2019-03-15 11:32:36 -0700

    writing tests

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -1,1 +1,11 @@
+import { GalacticAge } from "../src/backEnd.js";
 
+describe('GalacticAge', function() {
+
+  it('should count the number of vowels in a word', function() {
+    //Test content will go here.
+    const haiku = new Haiku('tiim', 'he', 'did');
+    expect(haiku.findVowels(haiku.first)).toEqual(2);
+  });
+
+});
--- src/backEnd.js
+++ src/backEnd.js
@@ -1,1 +1,15 @@
+//Haiku constructor
+export class GalacticAge {
+  constructor (year, month, gender) {
+    this.year = year;
+    this.month = month;
+    this.userGender = gender;
 
+    this.userAge = new Date(this.year, this.month);
+    this.maleExpectancy = new Date(43, 00);
+    this.femaleExpectancy = new Date(41, 00);
+  }
+
+  todaysDate ()
+
+}
